6471c636560a3f1547cf5a181c6da3d2b090850e
renaupadhyay <renaupadhyay01@gmail.com>
2018-08-14 17:54:18 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get "/name" do 
+    "Hello my name is Rena"
+end
+  get "/hometown" do
+    "My hometown is St Petersburg"
+  end
+  
+  get "/favorite-song" do
+    "My favorite song is Baby by Justin Bieber"
+  end
+  
 end
